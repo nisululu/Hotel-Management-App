a93924e5223499766612ed27ec95237b9df35de1
N33XAL <nischaltandukar0@gmail.com>
2024-07-09 11:55:33 +0545

    Creating necessary starter files and setting up styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import styled from "styled-components";
+
 function App() {
-  return <></>;
+  const H1 = styled.h1`
+    font-size: 30px;
+    font-weight: 600;
+  `;
+  return (
+    <div>
+      <H1>Hello world</H1>
+    </div>
+  );
 }
 
 export default App;
