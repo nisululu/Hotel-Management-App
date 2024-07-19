527e948ad1a96796f684314dcfe63b489153e1a9
N33XAL <nischaltandukar0@gmail.com>
2024-07-19 12:33:16 +0545

    Implemented pre-fetching react query functionality in bookings table

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,8 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../utils/constants";
 
 function useBookings() {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
 
   //Filter
@@ -21,10 +23,25 @@ function useBookings() {
   // Pagination
   const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
 
+  // Query
   const { data, isLoading, error } = useQuery({
     queryFn: () => getBookings({ filter, sortBy, page }),
     queryKey: ["bookings", filter, sortBy, page],
   });
+
+  // Pre-Fetching
+  const pageCount = Math.ceil(data?.count / PAGE_SIZE);
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+      queryKey: ["bookings", filter, sortBy, page + 1],
+    });
+
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+      queryKey: ["bookings", filter, sortBy, page - 1],
+    });
 
   return { data, isLoading, error };
 }
